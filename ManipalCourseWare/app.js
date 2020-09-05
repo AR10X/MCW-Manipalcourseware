4f0ba92516c945676d064ea9efa1f15a853b88db
AR10X <arehamanofficial@gmail.com>
2020-09-05 12:22:41 +0530

    nodejs backend added

---
+++ ManipalCourseWare/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app =express();
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//listen to port
+app.listen(3000);
+console.log('You are listening to port 3000...');
