e1442b494ebfc5f7dfc264f41b0d1fe025964137
AR10X <arehamanofficial@gmail.com>
2020-09-07 21:30:27 +0530

    added node routing

--- ManipalCourseWare/app.js
+++ ManipalCourseWare/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var controllerOne = require('./controllers/controllerOne');
 
-var app =express();
+var app = express();
 
 //set up template engine
 app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
+
+//fire controllers
+controllerOne(app);
 
 //listen to port
 app.listen(3000);
