9691b7cc7e285f694c1efef29662a3ee668ab8f3
AR10X <arehamanofficial@gmail.com>
2022-12-24 22:34:50 +0530

    Delete crowdsource.html and routing fixes

--- ManipalCourseWare/controllers/controllerOne.js
+++ ManipalCourseWare/controllers/controllerOne.js
@@ -3,5 +3,10 @@ module.exports = function(app){
     app.get('/explore',function(req, res){
         res.render('explore');
         });
-
+    
+    app.get('/video-page', function(req, res){
+        res.render('video-page');
+    });
+   
 };
+
